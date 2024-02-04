f0031b3eeedc7a3d8ed1ded0280fbe112d7b3a9e
fcphysics <felipecosta.physics@gmail.com>
2024-02-04 20:44:00 +0000

    Backbone computation via percolation

--- distanceclosure/backbone.py
+++ distanceclosure/backbone.py
@@ -9,7 +9,7 @@ These algorithms work with edges weighted as distances.
 
 import numpy as np
 import networkx as nx
-from distanceclosure.dijkstra import single_source_dijkstra_path_length
+from distanceclosure.dijkstra import single_source_dijkstra_path_length, single_source_target_dijkstra_path_length
 from networkx.algorithms.shortest_paths.weighted import _weight_function
 
 __name__ = 'distanceclosure'
@@ -18,9 +18,9 @@ __author__ = """\n""".join(['Rion Brattig Correia <rionbr@gmail.com>', 'Felipe X
 __all__ = [
     "backbone",
     "metric_backbone",
-    "ultrametric_backbone"
+    "ultrametric_backbone",
+    "backbone_percolation"
 ]
-
 
 __kinds__ = ['metric', 'ultrametric']
 __algorithms__ = ['dense', 'dijkstra']
@@ -146,6 +146,71 @@ def _compute_backbone(D, weight='weight', disjunction=sum, distortion=False, sel
                 G.remove_edge(n, v)
     
     return G, s_values
+
+def backbone_percolation(D, weight='weight', disjunction=sum, distortion=False, self_loops=False, *args, **kwargs):
+    """
+    Fast backbone (only) computation inspired by percolation.
+
+    Parameters
+    ----------
+    D : NetworkX graph
+        The Distance graph
+    weight : str, optional
+        Edge property containing distance values, by default 'weight'
+    disjunction: function (default=sum)
+        Whether to sum paths or use the max value.
+        Use `sum` for metric and `max` for ultrametric.
+    self_loops : bool, optional
+        If the distance graph has nodes with self distance greater than zero, by default False
+    distortion : bool, optional
+        If one wants to track semi-triangular distortion, by default False
+    cutoff : _type_, optional
+        Maximum number of connections in the path. If None, compute the entire closure as is the cutoff is the number of nodes, by default None
+    verbose : bool, optional
+        Prints statements as it computes, by default False
+
+    Returns
+    -------
+    NetworkX graph
+        The backbone subgraph.
+
+    Raises
+    ------
+    NotImplementedError
+        Self-loop closure and finite step (cutoff) not implemented yet
+    
+    """
+
+    if self_loops:
+        raise NotImplementedError
+    
+    G = D.copy()
+    weight_function = _weight_function(G, weight)
+
+    edges_analyzed = []
+    sorted_edges = sorted(G.edges(data=weight), key= lambda x: x[2], reverse=True)
+    s_values = dict() # Distortion dictionary
+
+    for u, v, _ in sorted_edges:
+        if (u, v) in edges_analyzed:
+            continue
+        else:
+            edges_analyzed.append((u, v))
+            dist, path = single_source_target_dijkstra_path_length(G=G, source=u, target=v, 
+                                                                   weight_function=weight_function, disjunction=disjunction)
+            if len(path) > 2:
+                
+                for i in range(len(path)-1):
+                    edges_analyzed.append((path[i], path[i+1]))
+                
+                # Compute distortion of the removed edge
+                s_values[(u, v)] = G[u][v]['distance']/dist
+                G.remove_edge(u, v)
+    
+    if distortion:
+        return G, s_values
+    else:
+        return G
 
 
 def _check_for_kind(kind):
