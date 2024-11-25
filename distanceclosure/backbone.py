045558511d9fb89a2aead3d70734bcbd349459e1
Felipe Xavier Costa <felipecosta.physics@gmail.com>
2024-11-25 10:37:36 +0000

    Updated heuristic code based on Kalavri 2016

--- distanceclosure/backbone.py
+++ distanceclosure/backbone.py
@@ -80,6 +80,8 @@ def iterative_backbone(D, weight='weight', kind='metric', distortion=False, self
         disjunction = sum
     elif kind == 'ultrametric':
         disjunction = max
+    elif kind == 'drastic':
+        disjunction=drastic_disjunction
     
     G = D.copy()
     weight_function = _weight_function(G, weight)
@@ -94,7 +96,7 @@ def iterative_backbone(D, weight='weight', kind='metric', distortion=False, self
         if verbose:
             i += 1
             per = i/total
-            print("Backbone: Dijkstra: {i:d} of {total:d} ({per:.2%})".format(i=i, total=total, per=per))
+            print("Backbone: Iterative Dijkstra: {i:d} of {total:d} ({per:.2%})".format(i=i, total=total, per=per))
         
         metric_dist = single_source_dijkstra_path_length(G, source=u, weight_function=weight_function, disjunction=disjunction)        
         if metric_dist[v] < dist:
@@ -106,7 +108,7 @@ def iterative_backbone(D, weight='weight', kind='metric', distortion=False, self
     else:
         return G
 
-def flagged_backbone(D, weight='weight', disjunction=sum, distortion=False, self_loops=False, *args, **kwargs):
+def flagged_backbone(D, weight='weight', kind='metric', distortion=False, self_loops=False, cutoff=None, verbose=False, *args, **kwargs):
     """
 
     Iterative backbone computation where edges are flagged as belonging to the backbone if they are part of an indirect shortest-path.
@@ -137,28 +139,46 @@ def flagged_backbone(D, weight='weight', disjunction=sum, distortion=False, self
     
     """
 
+    _check_for_kind(kind)
+    
     if self_loops:
         raise NotImplementedError
+    if cutoff is not None:
+        raise NotImplementedError
     
+    if kind == 'metric':
+        disjunction = sum
+    elif kind == 'ultrametric':
+        disjunction = max
+    elif kind == 'drastic':
+        disjunction=drastic_disjunction
+        
     G = D.copy()
     weight_function = _weight_function(G, weight)
 
     edges_analyzed = []
     sorted_edges = sorted(G.edges(data=weight), key= lambda x: x[2], reverse=True)
+    
+    if verbose:
+        total = G.number_of_edges()
 
     for u, v, _ in sorted_edges:
         if (u, v) in edges_analyzed:
             continue
         else:
             edges_analyzed.append((u, v))
-            dist, path = single_source_target_dijkstra_path(G=G, source=u, target=v, 
-                                                                   weight_function=weight_function, disjunction=disjunction)
+            
+            path = single_source_target_dijkstra_path(G=G, source=u, target=v, weight_function=weight_function, disjunction=disjunction)
             if len(path) > 2:
                 
                 for i in range(len(path)-1):
                     edges_analyzed.append((path[i], path[i+1]))
-                
+                    
                 G.remove_edge(u, v)
+            
+            if verbose:
+                per = len(edges_analyzed)/total
+                print("Backbone: Flagged Dijkstra: {nedges:d} of {total:d} ({per:.2%})".format(nedges=len(edges_analyzed), total=total, per=per))            
     
     if distortion:
         svals = _compute_distortions(D, weight=weight, disjunction=disjunction, distortion=distortion, *args, **kwargs)
@@ -186,9 +206,9 @@ def _compute_distortions(D, B, weight='weight', disjunction=sum, self_loops=Fals
     
     
 
-def heuristic_backbone(D, weight='weight', kind='metric', triangles=True, verbose=False, *args, **kwargs):
+def heuristic_backbone(D, weight='weight', kind='metric', distortion=False, self_loops=False, cutoff=None, verbose=False, *args, **kwargs):
     """
-    Heuristic backbone computation as described in: "V. Kalavri et al (2016) Proceedings of the VLDB Endowment, Volume 9, Issue 9"
+    Heuristic backbone computation combining triangle search (based on "V. Kalavri et al (2016) Proceedings of the VLDB Endowment, Volume 9, Issue 9") with :func:`iterative_backbone`.
 
     Parameters
     ----------
@@ -196,28 +216,65 @@ def heuristic_backbone(D, weight='weight', kind='metric', triangles=True, verbos
         The Distance graph
     weight : str, optional
         Edge property containing distance values, by default 'weight'
-    kind : str, optional
-        Distance accumulation kind. Either metric (sum) or ultrametric (max), by default 'metric'
-    verbose : bool, optional
-        Prints statements as it computes, by default False
+    disjunction: function (default=sum)
+        Whether to sum paths or use the max value.
+        Use `sum` for metric and `max` for ultrametric.
+    distortion : bool, optional
+        If one wants to track semi-triangular distortion, by default False
+    self_loops : bool, optional
+        If the distance graph has nodes with self distance greater than zero, by default False
 
     Returns
     -------
     NetworkX graph
         The backbone subgraph.
 
+    Raises
+    ------
+    NotImplementedError
+        Self-loop closure and finite step (cutoff) not implemented yet
+
     """
     
     _check_for_kind(kind)
     
-    if kind == 'metric':
-        G = _compute_heuristic_backbone(D, weight=weight, disjunction=sum, triangles=triangles, verbose=verbose, *args, **kwargs)
-    elif kind == 'ultrametric':
-        G = _compute_heuristic_backbone(D, weight=weight, disjunction=max, triangles=triangles, verbose=verbose, *args, **kwargs)
-    elif kind == 'drastic':
-        G = _compute_heuristic_backbone(D, weight=weight, disjunction=drastic_disjunction, triangles=triangles, verbose=verbose, *args, **kwargs)
+    if self_loops:
+        raise NotImplementedError
+    if cutoff is not None:
+        raise NotImplementedError
     
-    return G
+    if kind == 'metric':
+        disjunction = sum
+    elif kind == 'ultrametric':
+        disjunction = max
+    elif kind == 'drastic':
+        disjunction=drastic_disjunction
+        
+    G = D.copy()
+    weight_function = _weight_function(G, weight)
+
+    sorted_edges = sorted(G.edges(data=weight), key= lambda x: x[2], reverse=True)
+    
+    for u, v, dist in sorted_edges:
+
+        metric_dist_v = dist        
+        neighbors = list(G.neighbors(u)) # Need to be separate or will raise changing list error        
+        for k in neighbors:
+            if G.has_edge(k, v):
+                metric_dist_v = min(metric_dist_v, disjunction([G[u][k][weight], G[k][v][weight]]))
+
+        if metric_dist_v < dist:
+            G.remove_edge(u, v)
+        else:
+            metric_dist = single_source_dijkstra_path_length(G, source=u, weight_function=weight_function, disjunction=disjunction)            
+            if metric_dist[v] < dist:
+                G.remove_edge(u, v)    
+    
+    if distortion:
+        svals = _compute_distortions(D, weight=weight, disjunction=disjunction, distortion=distortion, *args, **kwargs)
+        return G, svals
+    else:
+        return G
 
 
 def _compute_heuristic_backbone(D, weight='weight', disjunction=sum, triangles=True, verbose=False, *args, **kwargs):
