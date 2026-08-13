2717bcd5fd2f703789857a74b11023791285a951
Felipe X Costa <felipecosta.physics@gmail.com>
2026-08-13 16:07:41 +0100

    Upload dombi synthesis parameter

--- distanceclosure/edge_measures.py
+++ distanceclosure/edge_measures.py
@@ -6,81 +6,162 @@ Edge Measures from Distance Closure
 ==================
 """
 
-from distanceclosure.backbone import iterative_backbone
+from distanceclosure import distance_closure
+from distanceclosure.backbone import metric_backbone, ultrametric_backbone
 import networkx as nx
 import numpy as np
+import multiprocessing as mp
 
-def metric_distortion(D, weight='weight'):
+
+def edge_distortion(D, weight='weight', kind='metric', self_loops=False, cutoff=None, verbose=False, *args, **kwargs):
     
-    G, svals = iterative_backbone(D, weight=weight, kind='metric', distortion=True)
+    if kind == 'metric':
+        G, svals = metric_backbone(D, weight=weight, distortion=True, self_loops=self_loops, cutoff=cutoff, verbose=verbose, *args, **kwargs)
+    elif kind == 'ultrametric':
+        G, svals = ultrametric_backbone(D, weight=weight, distortion=True, self_loops=self_loops, cutoff=cutoff, verbose=verbose, *args, **kwargs)
+    else:
+        raise ValueError("Invalid kind. Choose 'metric' or 'ultrametric'.")
+
     for u, v in G.edges():
         svals[(u, v)] = 1.0
         
-    nx.set_edge_attributes(D, values=svals, name='metric_distortion')
+    nx.set_edge_attributes(D, values=svals, name=f'{kind}_distortion')
     
     return D
 
 
-def ultrametric_distortion(D, weight='weight'):
-    
-    G, svals = iterative_backbone(D, weight=weight, kind='ultrametric', distortion=True)
-    for u, v in G.edges():
-        svals[(u, v)] = 1.0
-        
-    nx.set_edge_attributes(D, values=svals, name='ultrametric_distortion')
-    
-    return D
+def below_average_ratio(D, weight='weight', kind='metric', self_loops=False, cutoff=None, verbose=False, *args, **kwargs):
+    """
+    Computes below-average ratios for each edge with infinite distance, thus not existing in the original distance graph.
+    The formal definition is as follow:
+
+    .. math::
+        b_{ij} = <d_{ik}> / d_{ij}^m
+
+        b_{ji} = <d_{jk}> / d_{ij}^m
+
+    which is the average distance of all edges that leaves from node `x_i`, divided by its metric distance closure.
+    Note that `b_{ij}` can be different from `b_{ji}`.
+
+    Parameters
+    ----------
+    D (networkx.Graph): The original distance graph.
+    weight (string): Edge attribute containing distance values. Defaults to 'weight'.
+    kind (string): The type of distance closure to use. Defaults to 'metric'.
+    self_loops (bool): Whether to include self-loops in the computation. Defaults to False.
+    cutoff (float): The maximum distance to consider. Defaults to None.
+    verbose (bool): Whether to print progress information. Defaults to False.
+
+    Note
+    ----
+    Both arguments must be numpy arrays as the Metric Closure network is a dense matrix.
+
+    Warning
+    -------
+    This computation takes a while.
+    """
+
+    GC = distance_closure(D, kind=kind, algorithm='dijkstra', weight=weight, only_backbone=False, verbose=verbose, *args, **kwargs)
+
+    if GC.is_directed():
+        sout = GC.out_degree(weight=weight)
+        kout = GC.out_degree()
+    else:
+        sout = GC.degree(weight=weight)
+        kout = GC.degree()
+
+    mean_distance = {k: sout[k] / kout[k] if kout[k] > 0 else 0 for k in GC.nodes()}
+
+    G = nx.DiGraph()
+    G.add_nodes_from(GC.nodes(data=True))
+
+    for u, v, d in GC.edges(data=True):
+        if not D.has_edge(u, v):
+
+            G.add_edge(u, v, **d)
+
+    dict_b_ij_values = {
+        (i, j): mean_distance[i] / d.get(weight_metric_distance)
+        for i, j, d in G.edges(data=True)
+        if (d.get(weight_distance) == np.inf)
+    }
+    nx.set_edge_attributes(G, name='b_ij-value', values=dict_b_ij_values)
+
+    dict_b_ji_values = {
+        (i, j): mean_distance[j] / d.get(weight_metric_distance)
+        for i, j, d in G.edges(data=True)
+        if (d.get(weight_distance) == np.inf)
+    }
+    nx.set_edge_attributes(G, name='b_ji-value', values=dict_b_ji_values)
+
+    return G
 
 
-def dombi_synthesis(D, prox_weight='weight', L=1e-3, R=1e3):
-    
+def dombi_synthesis(D, prox_weight='weight', L=1e-4, R=1e3, ntrials=50, ncpu=1):
+
     G = D.copy()
     nx.set_edge_attributes(G, name='distance', values={(u, v): (1/w - 1) for u, v, w in G.edges(data=prox_weight)})
-    U = iterative_backbone(G, weight='distance', kind='ultrametric', distortion=False)
+    U = ultrametric_backbone(G, weight='distance', distortion=False)
     
-    sorted_edges = merge_sort_dbs(G, L, R, U)
-    print(sorted_edges)
-    nx.set_edge_attributes(G, name='dombi_synthesis', values={key: val for key, val in sorted_edges})
-    
-    return G
-    
-    
-def merge_sort_dbs(G, L, R, U):
-    
-    mid = np.sqrt(L*R)
-    nx.set_edge_attributes(G, name='new_distance', values={(u, v): d**mid for u, v, d in G.edges(data='distance')})
-    B = iterative_backbone(G, weight='new_distance', kind='metric', distortion=False)
-    
-    if len(set(B.edges())-set(U.edges())) == 0: # type: ignore
-        return [R]
-    if len(set(G.edges())-set(B.edges())) == 0: # type: ignore
-        return [L]
-    if len(set(G.edges())-set(B.edges())-set(U.edges())) == 1: # type: ignore
-        return [mid]
-    
-    
-    left_edges = merge_sort_dbs(G, L, mid, U)    
-    right_edges = merge_sort_dbs(B, mid, R, U)
-    
-    return merge(left_edges, right_edges)
+    ultrametric_edges = list(U.edges())
+    non_ultrametric_edges = list(set(G.edges()) - set(U.edges()))
+
+    nr_non_ultrametric_edges = len(non_ultrametric_edges)
+
+    #Parallelized computation of the lambdas
+    inputs = zip([G for _ in range(nr_non_ultrametric_edges)],
+                 [edge for edge in non_ultrametric_edges], 
+                 [L for _ in range(nr_non_ultrametric_edges)],
+                 [R for _ in range(nr_non_ultrametric_edges)],
+                 [ntrials for _ in range(nr_non_ultrametric_edges)])
+
+    with mp.Pool(processes=ncpu) as pool:
+        non_ultrametric_edges_lambdas = pool.starmap(robust_get_largest_lambda_with_edge_in_backbone, inputs)
+        pool.close()
+
+    #Create dictionary that assigns a lambda to each edge
+    edges = non_ultrametric_edges + ultrametric_edges
+    lambdas = non_ultrametric_edges_lambdas + [np.inf for _ in range(len(ultrametric_edges))]
+    edge_to_lambda = dict(zip(edges, lambdas))
+
+    #Save the Edge:Lambda correspondence as a network with lambdas in the edge attributes
+    nx.set_edge_attributes(D, edge_to_lambda, name=f'synthesis_parameter')
+
+    return D
 
 
-def merge(left, right):
-    result = []
-    i = 0  # Pointer for the left list
-    j = 0  # Pointer for the right list
+def robust_get_largest_lambda_with_edge_in_backbone(net, edge, L, R, ntrial):
 
-    # Compare elements from both lists and append the smaller one
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
+    res='None'
+    for l in np.arange(L, R, 5):
+        r=l+5
 
-    # If there are remaining elements in left or right, append them
-    result.extend(left[i:])
-    result.extend(right[j:])
+        l_val, r_val = is_edge_in_backbone(net, l, edge), is_edge_in_backbone(net, r, edge)
+        #I want to compute the largest l such that the edge is in the backbone, using a binary search with k steps
+        if l_val==True and r_val==False:
+            for _ in range(ntrial):
+                mid = (l+r)/2
+                mid_val = is_edge_in_backbone(net, mid, edge)
+                if mid_val == True:
+                    l = mid
+                else:
+                    r = mid
+            res = l
+            break
+        
+    if res=='None':
+        print('The given initial bounds do not give any information with a initial search of width 5 | L:', L, 'R:', R)
+        
+    #print(f'Edge {edge} | Lambda:', res)
+    return res
 
-    return list(result)
+
+def is_edge_in_backbone(net, l, edge):
+
+    nx.set_edge_attributes(net, name='new_distance', values={(u, v): d**l for u, v, d in net.edges(data='distance')})
+    sp = nx.shortest_path(net,  edge[0], edge[1], weight='new_distance')
+
+    if len(sp)==2: 
+        return True
+    else: 
+        return False
