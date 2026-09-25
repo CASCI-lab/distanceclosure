Fix duplicated code and speed up in closure computation

distanceclosure/closure.py
@@ -94,6 +94,7 @@ def _closure(D: nx.Graph | nx.DiGraph, kind: str, disjunction: Callable, weight:
     G = D.copy() 
     edges_seen = set()
     total = G.number_of_nodes()
+    edges_to_add = []
 
     i = 1
     for u, lengths in all_pairs_dijkstra_path_length(G, weight=weight, disjunction=disjunction):
@@ -110,17 +111,10 @@ def _closure(D: nx.Graph | nx.DiGraph, kind: str, disjunction: Callable, weight:
 
                 if not G.has_edge(u, v):
                     if not existing_edges_only:
-                        G.add_edge(u, v, **{weight: np.inf, kind_distance: length})
+                        edges_to_add.append((u, v, {weight: np.inf, kind_distance: length}))
                 else:
-                    edges_seen.add((u, v))
-                    kind_distance = '{kind:s}_distance'.format(kind=kind)
-                    is_kind = 'is_{kind:s}'.format(kind=kind)
-                    if not G.has_edge(u, v):
-                        if not only_backbone:
-                            G.add_edge(u, v, **{weight: np.inf, kind_distance: length})
-                    else:
-                        G[u][v][kind_distance] = length
-                        G[u][v][is_kind] = True if (length == G[u][v][weight]) else False
+                    G[u][v][kind_distance] = length
+                    G[u][v][is_kind] = True if (length == G[u][v][weight]) else False
             i += 1
 
     if self_loops:
@@ -135,13 +129,17 @@ def _closure(D: nx.Graph | nx.DiGraph, kind: str, disjunction: Callable, weight:
                 spl = disjunction([G[u][k][weight], disjunction(G[return_path[idx-1]][return_path[idx]][weight] for idx in range(1, len(return_path)))])
                 if spl < length:
                     length = spl
-
+            
+            kind_distance = '{kind:s}_distance'.format(kind=kind)
+            is_kind = 'is_{kind:s}'.format(kind=kind)
             if not G.has_edge(u, u):
                 if not existing_edges_only:
-                    G.add_edge(u, u, **{weight: np.inf, kind_distance: length})
+                    edges_to_add.append((u, u, {weight: np.inf, kind_distance: length}))
             else:
                 G[u][u][kind_distance] = length
                 G[u][u][is_kind] = True if (length == G[u][u][weight]) else False
+    
+    G.add_edges_from(edges_to_add)
 
     return G
 